a399eba4ba73c93aee50765b77fa936fc06b0837
Roger Santos <luan.roger.2003@gmail.com>
2019-02-24 13:13:24 -0300

    Correções de erros e adição do menu.

--- JogoDaAdivinhação/Program.cs
+++ JogoDaAdivinhação/Program.cs
@@ -12,26 +12,62 @@ namespace JogoDaAdivinhação
         {
             // Boas vindas
             Console.WriteLine("Bem-vindo(a) ao jogo da adivinhação");
+
+            // Menu
+            Console.WriteLine("[ 1 ] Iniciar jogo.     [ 2 ] Sair");
+            int menuEscolha = Convert.ToInt32(Console.ReadLine());
+
+            if (menuEscolha == 1)
+            {
+
+            }
+            else if (menuEscolha == 2)
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Você digitou um número inválido");
+                return;
+            }
+            //----------------------------------------------------------------
             Console.WriteLine("Estou pensando em um número entre 1 e 5.");
-            Console.WriteLine("Tente adivinhar que número é este.");
+            Console.WriteLine("Tente adivinhar que número é este. (0 para sair do jogo.)");
             // -----------------------------------------------------------------
             while (true)
             {
                 // Gerador de números.
                 Random random = new Random();
-                int nunRandom = random.Next(1, 5);
+                string nunRandom = random.Next(1, 6).ToString();
                 // --------------------------------
                 Console.WriteLine("============================================");
-                int nunEscolhido = Convert.ToInt32(Console.ReadLine());
+                string nunEscolhido = Console.ReadLine();
+
+                // Anti-bug
+                bool error = false;
+                if (nunEscolhido == "")
+                {
+                    error = true;
+                }
+                //-------------------------------------------------------
 
                 // Condições para determinar se o jogador venceu ou não.
-                if (nunEscolhido == nunRandom)
+                if (nunEscolhido == nunRandom && error == false)
                 {
                     Console.WriteLine("Parabéns, você acertou.");
                 }
-                else
+                else if (error == true)
+                {
+                    Console.WriteLine("Você digitou um número inválido");
+                }
+                else if (nunEscolhido != nunRandom)
                 {
                     Console.WriteLine("Você errou, tente novamente.");
+                    Console.WriteLine($"O número que estava pensando era {nunRandom}");
+                }
+                else if (nunEscolhido == "0")
+                {
+                    Environment.Exit(0);
                 }
                 // -------------------------------------------------------------
             }
