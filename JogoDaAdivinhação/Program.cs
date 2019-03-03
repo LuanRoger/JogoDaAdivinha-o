63abde0c7255b58f30ade823357cd8e70d9a9581
Roger Santos <luan.roger.2003@gmail.com>
2019-03-03 10:35:01 -0300

    Versão 1.0 Correções realizadas; Sistema de pontuação implementado; Controle de tempo implementado.

--- JogoDaAdivinhação/Program.cs
+++ JogoDaAdivinhação/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace JogoDaAdivinhação
 {
@@ -12,14 +13,16 @@ namespace JogoDaAdivinhação
         {
             // Boas vindas
             Console.WriteLine("Bem-vindo(a) ao jogo da adivinhação");
+            //-----------------------------------------------------------
 
             // Menu
             Console.WriteLine("[ 1 ] Iniciar jogo.     [ 2 ] Sair");
             int menuEscolha = Convert.ToInt32(Console.ReadLine());
+            Thread.Sleep(3000);
 
             if (menuEscolha == 1)
             {
-
+                
             }
             else if (menuEscolha == 2)
             {
@@ -33,13 +36,18 @@ namespace JogoDaAdivinhação
             //----------------------------------------------------------------
             Console.WriteLine("Estou pensando em um número entre 1 e 5.");
             Console.WriteLine("Tente adivinhar que número é este. (0 para sair do jogo.)");
-            // -----------------------------------------------------------------
+            //-----------------------------------------------------------------
+
+            // Variaveis
+            int pontuacao = 0;
+            int total = 0;
+            //---------------------------
             while (true)
             {
                 // Gerador de números.
                 Random random = new Random();
                 string nunRandom = random.Next(1, 6).ToString();
-                // --------------------------------
+                //--------------------------------
                 Console.WriteLine("============================================");
                 string nunEscolhido = Console.ReadLine();
 
@@ -51,25 +59,32 @@ namespace JogoDaAdivinhação
                 }
                 //-------------------------------------------------------
 
-                // Condições para determinar se o jogador venceu ou não.
+                // Condições.
                 if (nunEscolhido == nunRandom && error == false)
                 {
                     Console.WriteLine("Parabéns, você acertou.");
+                    pontuacao = pontuacao + 1;
+                    total = total + 1;
                 }
                 else if (error == true)
                 {
                     Console.WriteLine("Você digitou um número inválido");
+                    error = false;
                 }
-                else if (nunEscolhido != nunRandom)
+                else if (nunEscolhido != nunRandom && error == false && nunEscolhido != "0")
                 {
                     Console.WriteLine("Você errou, tente novamente.");
                     Console.WriteLine($"O número que estava pensando era {nunRandom}");
+                    pontuacao = pontuacao - 1;
+                    total = total + 1;
                 }
                 else if (nunEscolhido == "0")
                 {
+                    Console.WriteLine($"Sua pontuação foi de {pontuacao}/{total}");
+                    Console.ReadKey();
                     Environment.Exit(0);
                 }
-                // -------------------------------------------------------------
+                //-------------------------------------------------------------
             }
         }
     }
